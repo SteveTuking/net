67c60ad57010473d4c866b01c1d8e5d5f458606a
zhangbing <zhangbing1@yonyou.com>
2018-05-09 08:49:03 +0800

    修改调整代码

--- cust-consumer/src/main/java/cn/cust/netty/client/ChannelManager.java
+++ cust-consumer/src/main/java/cn/cust/netty/client/ChannelManager.java
@@ -6,38 +6,38 @@ import java.util.concurrent.atomic.AtomicInteger;
 import io.netty.channel.ChannelFuture;
 
 public class ChannelManager {
-	  static  CopyOnWriteArrayList<String> realServerPath=new CopyOnWriteArrayList<String>();
-	  static AtomicInteger position=new AtomicInteger(0);
-	public static CopyOnWriteArrayList<ChannelFuture> channelFutures=new CopyOnWriteArrayList<>();
+    static CopyOnWriteArrayList<String> realServerPath = new CopyOnWriteArrayList<String>();
+    static AtomicInteger position = new AtomicInteger(0);
+    public static CopyOnWriteArrayList<ChannelFuture> channelFutures = new CopyOnWriteArrayList<>();
 
-	
-	public static void removeChannel(ChannelFuture channel){
-		channelFutures.remove(channel);
-	}
-	
-	public static void add(ChannelFuture channel){
-		channelFutures.add(channel);
-	}
-	
-	public static void clear(){
-		channelFutures.clear();
-	}
 
-	public static ChannelFuture get(AtomicInteger i) {
-		int size = channelFutures.size();
-		ChannelFuture channel = null;
-		if(i.get()>size){
-			channel = channelFutures.get(0);
-			ChannelManager.position=new AtomicInteger(1);
-		}else{
-			channel = channelFutures.get(i.getAndIncrement());
-		}
-		
-		if(!channel.channel().isActive()){
-			channelFutures.remove(channel);
-			return get(position);
-		}
-		
-		return channel;
-	}
+    public static void removeChannel(ChannelFuture channel) {
+        channelFutures.remove(channel);
+    }
+
+    public static void add(ChannelFuture channel) {
+        channelFutures.add(channel);
+    }
+
+    public static void clear() {
+        channelFutures.clear();
+    }
+
+    public static ChannelFuture get(AtomicInteger i) {
+        int size = channelFutures.size();
+        ChannelFuture channel = null;
+        if (i.get() > size) {
+            channel = channelFutures.get(0);
+            ChannelManager.position = new AtomicInteger(1);
+        } else {
+            channel = channelFutures.get(i.getAndIncrement());
+        }
+
+        if (!channel.channel().isActive()) {
+            channelFutures.remove(channel);
+            return get(position);
+        }
+
+        return channel;
+    }
 }
--- netty/src/main/java/cn/cust/netty/proxy/UserDao.java
+++
@@ -1,8 +0,0 @@
-package cn.cust.netty.proxy;
-
-/**
- * Created by zhangbing on 2018/3/29.
- */
-public interface UserDao {
-    public void queryName();
-}
